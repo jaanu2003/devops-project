db7e9cb8d1caefec0d607f420815492ceb20c071
Jahnavi Urugonda <93399446+jaanu2003@users.noreply.github.com>
2026-05-30 21:27:18 +0530

    modified generatory

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-cd ../terraform
+set -e
 
-IP=$(terraform output -raw instance_public_ip)
+cd ../terraform
+terraform output -raw instance_public_ip > /tmp/ip.txt
+IP=$(cat /tmp/ip.txt)
 
 cd ../ansible
 
 echo "[web]" > inventory
 echo "$IP ansible_user=ubuntu ansible_ssh_private_key_file=/home/jahnavi/keys/devops-key.pem" >> inventory
 
-echo "Inventory generated with IP: $IP"
+echo "Inventory generated: $IP"
+
+ansible-playbook -i inventory deploy.yml
